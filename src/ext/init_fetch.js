b39179f51843e480017f7bdeea4e68fc0e24e814
Richard Carson <r.carson@calian.com>
2023-10-24 11:43:50 -0400

    Minor fix to fetch, closes #18

--- src/ext/init_fetch.js
+++ src/ext/init_fetch.js
@@ -1,7 +1,3 @@
-import * as headers from "ext:deno_fetch/20_headers.js";
-import * as formData from "ext:deno_fetch/21_formdata.js";
-import * as request from "ext:deno_fetch/23_request.js";
-import * as response from "ext:deno_fetch/23_response.js";
 import * as fetch from "ext:deno_fetch/26_fetch.js";
 
 Deno.core.setWasmStreamingCallback(fetch.handleWasmStreaming);
