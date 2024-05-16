ad6d546aeff7ab63589c7d1e600b39fe8aa26125
Richard Carson <r.carson@calian.com>
2024-05-16 15:18:39 -0400

    update nix

--- src/ext/io/tty_unix.rs
+++ src/ext/io/tty_unix.rs
@@ -83,7 +83,10 @@ fn op_set_raw(state: &mut OpState, rid: u32, is_raw: bool, cbreak: bool) -> Resu
 
     let raw_fd = handle_or_fd;
 
-    fn wrap_fd<'a>(r: &'a ResourceTable, fd: RawFd) -> std::os::fd::BorrowedFd<'a> {
+    fn wrap_fd<'a>(
+        r: &'a deno_core::ResourceTable,
+        fd: std::os::fd::RawFd,
+    ) -> std::os::fd::BorrowedFd<'a> {
         match fd {
             -1 => Err(anyhow!("bad file descriptor")),
             _ => unsafe { std::os::fd::BorrowedFd::borrow_raw(fd) },
