55f56f693ea7e4247f86ff155fad07e90f965276
Richard Carson <r.carson@calian.com>
2024-06-17 14:43:55 -0400

    Add options for extensions

--- src/ext/crypto/mod.rs
+++ src/ext/crypto/mod.rs
@@ -6,14 +6,14 @@ extension!(
     esm = [ dir "src/ext/crypto", "init_crypto.js" ],
 );
 
-pub fn extensions(seed: Option<i64>) -> Vec<Extension> {
+pub fn extensions(seed: Option<u64>) -> Vec<Extension> {
     vec![
         deno_crypto::deno_crypto::init_ops_and_esm(seed),
         init_crypto::init_ops_and_esm(),
     ]
 }
 
-pub fn snapshot_extensions(seed: Option<i64>) -> Vec<Extension> {
+pub fn snapshot_extensions(seed: Option<u64>) -> Vec<Extension> {
     vec![
         deno_crypto::deno_crypto::init_ops(seed),
         init_crypto::init_ops(),
