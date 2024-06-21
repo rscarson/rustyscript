17f1fcc9e27428a501e918d74f598a36c9f3ba2c
Richard Carson <r.carson@calian.com>
2024-06-21 14:55:43 -0400

    correct callback utils

--- src/utilities.rs
+++ src/utilities.rs
@@ -137,7 +137,7 @@ mod runtime_macros {
     /// ```
     #[macro_export]
     macro_rules! sync_callback {
-        (|$($arg:ident: $arg_ty:ty),*| $body:block) => {
+        (|$($arg:ident: $arg_ty:ty),*| $body:expr) => {
             |args: &[$crate::serde_json::Value]| {
                 let mut args = args.iter();
                 $(
@@ -166,7 +166,7 @@ mod runtime_macros {
     /// ```
     #[macro_export]
     macro_rules! async_callback {
-        (|$($arg:ident: $arg_ty:ty),*| $body:block) => {
+        (|$($arg:ident: $arg_ty:ty),*| $body:expr) => {
             |args: Vec<$crate::serde_json::Value>| Box::pin(async move {
                 let mut args = args.iter();
                 $(
@@ -193,7 +193,7 @@ mod test_runtime {
     fn test_callback() {
         let add = sync_callback!(|a: i64, b: i64| { Ok::<i64, Error>(a + b) });
 
-        let add2 = async_callback!(|a: i64, b: i64| { async move { Ok::<i64, Error>(a + b) } });
+        let add2 = async_callback!(|a: i64, b: i64| async move { Ok::<i64, Error>(a + b) });
 
         let args = vec![
             serde_json::Value::Number(5.into()),
