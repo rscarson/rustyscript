ed48d5a652447b673ab419411b7f45d610779b8d
kovaszab <kszabi109@gmail.com>
2024-07-13 09:10:59 +0200

    Remove redefinition of CloseEvent

--- src/ext/websocket/init_websocket.js
+++ src/ext/websocket/init_websocket.js
@@ -1,9 +1,8 @@
 import * as websocket from "ext:deno_websocket/01_websocket.js";
 import * as websocketStream from "ext:deno_websocket/02_websocketstream.js";
 
-import { applyToGlobal, getterOnly, nonEnumerable } from 'ext:rustyscript/rustyscript.js';
+import { applyToGlobal, nonEnumerable } from 'ext:rustyscript/rustyscript.js';
 
 applyToGlobal({
     WebSocket: nonEnumerable(websocket.WebSocket),
-    CloseEvent: nonEnumerable(websocket.CloseEvent)
 });
