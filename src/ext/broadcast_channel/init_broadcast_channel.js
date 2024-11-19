88e19cf404be278c656f70fd0c58e6cc51cb4881
Richard Carson <Rscarson@rogers.com>
2024-11-19 11:27:54 -0500

    Expose serializers for broadcast_channel

--- src/ext/broadcast_channel/init_broadcast_channel.js
+++ src/ext/broadcast_channel/init_broadcast_channel.js
@@ -1,6 +1,9 @@
 import * as broadcastChannel from "ext:deno_broadcast_channel/01_broadcast_channel.js";
+import { core } from "ext:core/mod.js";
 
 import { applyToGlobal, nonEnumerable } from 'ext:rustyscript/rustyscript.js';
 applyToGlobal({
     BroadcastChannel: nonEnumerable(broadcastChannel.BroadcastChannel),
+    serialize: nonEnumerable(core.serialize),
+    deserialize: nonEnumerable(core.deserialize),
 });
