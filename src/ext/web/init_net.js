60b534ff01eb542c04988a9fd352556c779811e1
Richard Carson <r.carson@calian.com>
2024-11-20 15:43:57 -0500

    typo

--- src/ext/web/init_net.js
+++ src/ext/web/init_net.js
@@ -2,7 +2,7 @@
 import * as net from "ext:deno_net/01_net.js";
 import * as tls from "ext:deno_net/02_tls.js";
 
-cglobalThis.Deno.connect = net.connect;
+globalThis.Deno.connect = net.connect;
 globalThis.Deno.listen = net.listen;
 globalThis.Deno.resolveDns = net.resolveDns;
 
