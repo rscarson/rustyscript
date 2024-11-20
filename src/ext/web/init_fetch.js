030e1d49f4defb9a623e341e66e10adbce723d2e
Richard Carson <Rscarson@rogers.com>
2024-11-20 16:43:06 -0500

    Update init_fetch.js

--- src/ext/web/init_fetch.js
+++ src/ext/web/init_fetch.js
@@ -1,5 +1,6 @@
 import * as headers from "ext:deno_fetch/20_headers.js";
 import * as formData from "ext:deno_fetch/21_formdata.js";
+import * as httpClient from "ext:deno_fetch/22_http_client.js";
 import * as request from "ext:deno_fetch/23_request.js";
 import * as response from "ext:deno_fetch/23_response.js";
 import * as fetch from "ext:deno_fetch/26_fetch.js";
