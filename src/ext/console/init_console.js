30e862b07437f049b6691996dd866a2c6a5b03f4
mocchira <nekotaroh@gmail.com>
2025-01-23 12:39:48 +0900

    chore(src/ext): make net/tls invisible and replace console.log impl

--- src/ext/console/init_console.js
+++ src/ext/console/init_console.js
@@ -3,7 +3,15 @@ import * as _console from 'ext:deno_console/01_console.js';
 import { applyToGlobal, nonEnumerable } from 'ext:rustyscript/rustyscript.js';
 applyToGlobal({
     console: nonEnumerable(
-      new _console.Console((msg, level) => globalThis.Deno.core.print(msg, level > 1)),
+      new _console.Console((msg, _level) => {
+          try {
+              rustyscript.functions['console.log'](msg);
+          } catch (_e) {
+              // ignore
+              // NOTE(ysh)
+              // This can happen while console.log is not registered
+          }
+      }),
     ),
 });
 
--- src/ext/web/init_net.js
+++ src/ext/web/init_net.js
@@ -1,20 +1,3 @@
 
 import * as net from "ext:deno_net/01_net.js";
 import * as tls from "ext:deno_net/02_tls.js";
-
-globalThis.Deno.connect = net.connect;
-globalThis.Deno.listen = net.listen;
-globalThis.Deno.resolveDns = net.resolveDns;
-
-import {
-    op_net_listen_udp,
-    op_net_listen_unixpacket,
-} from "ext:core/ops";
-globalThis.Deno.listenDatagram = net.createListenDatagram(
-    op_net_listen_udp,
-    op_net_listen_unixpacket,
-);
-
-globalThis.Deno.connectTls = tls.connectTls;
-globalThis.Deno.listenTls = tls.listenTls;
-globalThis.Deno.startTls = tls.startTls;
